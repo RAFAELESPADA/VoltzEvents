b93b79f64a03464ddd2b2d402894607b46182f26
RAFAELESPADA <34714331+RAFAELESPADA@users.noreply.github.com>
2024-09-05 13:49:41 -0300

    Update Fake.java

--- src/pvp/sunshine/bukkit/manager/fake/Fake.java
+++ src/pvp/sunshine/bukkit/manager/fake/Fake.java
@@ -222,6 +222,7 @@ if (args.length < 2 && !args[0].equals("reset") && !args[0].equals("random") && 
 
         String fakeName = FakeNameGenerator.generateFakeName();
         String tag = getRandomTag();
+        FakeNameManager.setRealName(player.getUniqueId(), player.getName());
         changeName(fakeName, player);
         TagAPI.setNameTag(player.getName(), "team", availableTags.get(tag), " " + SQLClan.getTagPlayer(player));
         
